d9a546d41f49af9a75db9bd3d3dbf4659bf79611
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-10-11 19:29:24 -0400

    add borders to cards

--- Krypto/Krypto/RMDKryptoViewController.m
+++ Krypto/Krypto/RMDKryptoViewController.m
@@ -35,37 +35,54 @@
     self.targetLabel = [[UILabel alloc] init];
     self.targetLabel.translatesAutoresizingMaskIntoConstraints = NO;
     self.targetLabel.textAlignment = NSTextAlignmentCenter;
-    self.targetLabel.layer.borderColor = [UIColor blackColor].CGColor;
     [self.view addSubview:self.targetLabel];
     
     self.card1 = [[UILabel alloc] init];
     self.card1.translatesAutoresizingMaskIntoConstraints = NO;
     self.card1.textAlignment = NSTextAlignmentCenter;
+    self.card1.layer.borderColor = [UIColor blackColor].CGColor;
+    self.card1.layer.borderWidth = 2.0;
+    self.card1.layer.cornerRadius = 6.0;
     [self.view addSubview:self.card1];
     
     self.card2 = [[UILabel alloc] init];
     self.card2.translatesAutoresizingMaskIntoConstraints = NO;
     self.card2.textAlignment = NSTextAlignmentCenter;
+    self.card2.layer.borderColor = [UIColor blackColor].CGColor;
+    self.card2.layer.borderWidth = 2.0;
+    self.card2.layer.cornerRadius = 6.0;
     [self.view addSubview:self.card2];
     
     self.card3 = [[UILabel alloc] init];
     self.card3.translatesAutoresizingMaskIntoConstraints = NO;
     self.card3.textAlignment = NSTextAlignmentCenter;
+    self.card3.layer.borderColor = [UIColor blackColor].CGColor;
+    self.card3.layer.borderWidth = 2.0;
+    self.card3.layer.cornerRadius = 6.0;
     [self.view addSubview:self.card3];
     
     self.card4 = [[UILabel alloc] init];
     self.card4.translatesAutoresizingMaskIntoConstraints = NO;
     self.card4.textAlignment = NSTextAlignmentCenter;
+    self.card4.layer.borderColor = [UIColor blackColor].CGColor;
+    self.card4.layer.borderWidth = 2.0;
+    self.card4.layer.cornerRadius = 6.0;
     [self.view addSubview:self.card4];
     
     self.card5 = [[UILabel alloc] init];
     self.card5.translatesAutoresizingMaskIntoConstraints = NO;
     self.card5.textAlignment = NSTextAlignmentCenter;
+    self.card5.layer.borderColor = [UIColor blackColor].CGColor;
+    self.card5.layer.borderWidth = 2.0;
+    self.card5.layer.cornerRadius = 6.0;
     [self.view addSubview:self.card5];
     
     self.card6 = [[UILabel alloc] init];
     self.card6.translatesAutoresizingMaskIntoConstraints = NO;
     self.card6.textAlignment = NSTextAlignmentCenter;
+    self.card6.layer.borderColor = [UIColor blackColor].CGColor;
+    self.card6.layer.borderWidth = 2.0;
+    self.card6.layer.cornerRadius = 6.0;
     [self.view addSubview:self.card6];
     
     self.targetLabel.text = @"Target";
@@ -80,6 +97,7 @@
     UIStackView *innerStack = [[UIStackView alloc] initWithArrangedSubviews:@[self.card1, self.card2, self.card3, self.card4, self.card5, self.card6]];
     innerStack.translatesAutoresizingMaskIntoConstraints = NO;
     innerStack.distribution = UIStackViewDistributionFillEqually;
+    innerStack.spacing = 20;
     [self.view addSubview:innerStack];
     
     UIStackView *outerStack = [[UIStackView alloc] initWithArrangedSubviews:@[self.targetLabel, innerStack]];
@@ -89,7 +107,7 @@
     [self.view addSubview:outerStack];
     
     NSDictionary *views = @{@"stack": outerStack};
-    NSArray *vert = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[stack]-|"
+    NSArray *vert = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[stack]-20-|"
                                                             options:0
                                                             metrics:nil
                                                               views:views];
