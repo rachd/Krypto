cffb7ab032e6e103cd65e0028b5de3ec9a3e0249
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-10-10 23:24:35 -0400

    initial commit of view controller

---
+++ Krypto/Krypto/RMDKryptoViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMDKryptoViewController.h
+//  Krypto
+//
+//  Created by Rachel Dorn on 10/10/16.
+//  Copyright © 2016 RachelDorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMDKryptoViewController : UIViewController
+
+@end
---
+++ Krypto/Krypto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Krypto
+//
+//  Created by Rachel Dorn on 10/10/16.
+//  Copyright © 2016 RachelDorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
