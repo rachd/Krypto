1d8b4b92003bf008bef0db7cd1610a4e5a0dbf9f
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-10-22 16:19:23 -0400

    start easy mode button

--- Krypto/Krypto/RMDHomeViewController.m
+++ Krypto/Krypto/RMDHomeViewController.m
@@ -8,6 +8,7 @@
 
 #import "RMDHomeViewController.h"
 #import "RMDKryptoViewController.h"
+#import "RMDEasyViewController.h"
 
 @interface RMDHomeViewController ()
 
@@ -24,11 +25,22 @@
     hardMode.frame = CGRectMake(20, self.view.frame.size.height / 4, self.view.frame.size.width - 40, 40);
     [hardMode addTarget:self action:@selector(startHardMode) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:hardMode];
+    
+    UIButton *easyMode = [UIButton buttonWithType:UIButtonTypeSystem];
+    [easyMode setTitle:@"Easy Mode" forState:UIControlStateNormal];
+    easyMode.frame = CGRectMake(20, self.view.frame.size.height / 2, self.view.frame.size.width - 40, 40);
+    [easyMode addTarget:self action:@selector(startEasyMode) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:easyMode];
 }
 
 - (void)startHardMode {
     RMDKryptoViewController *hardKrypto = [[RMDKryptoViewController alloc] init];
     [self presentViewController:hardKrypto animated:YES completion:nil];
+}
+
+- (void)startEasyMode {
+    RMDEasyViewController *easyKrypto = [[RMDEasyViewController alloc] init];
+    [self presentViewController:easyKrypto animated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
